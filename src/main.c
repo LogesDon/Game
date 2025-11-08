d5de88d43d50677311e82439f59b33fdf1d9dd91
Logan <z5686994@ad.unsw.edu.au>
2025-11-09 09:18:55 +1100

    Documentation: setup main.c file with comments, main function and header library

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
-// Test file for game
+// main.c 
+// Written by Logan Donnelly
+// Date: 9/11/2025
+// Description: main file where the game will be run
+
+#include <src/ticTacToe.h> 
+
+int main(void) {
+
+    return 0; 
+}
+
