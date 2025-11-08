b1aef59af047b691353737d25fe614a7b380d253
Logan <z5686994@ad.unsw.edu.au>
2025-11-09 09:39:17 +1100

    Implementation: commented flow of game, added print statement to show game not yet implemented

--- src/main.c
+++ src/main.c
@@ -3,9 +3,25 @@
 // Date: 9/11/2025
 // Description: main file where the game will be run
 
-#include <src/ticTacToe.h> 
+#include "ticTacToe.h" 
 
 int main(void) {
+
+    printf("GAME NOT YET IMPLEMENTED.\n");
+
+    // Welcome Message + Explain Rules of Game to User
+
+    // Once rules of game explained ask user if they want to play 
+
+    // Once confirmed, initialise the board with n rows and n columns and 
+    // proceed to play. 
+
+    // If it is two player, take inputs from user only. If it is one player, 
+    // use random number generator to implement playing a computer. 
+
+    // Once player wins or loses, notify them as such. 
+
+    // Start off with best of three, or whatever game they want to play. 
 
     return 0; 
 }
